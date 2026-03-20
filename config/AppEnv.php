41189e647f0603446ec3ef95bd077c0401222460
Gagan Rana <gaganrana.rana@gmail.com>
2026-03-20 14:04:35 +0530

    chore: add AppEnv.php and constants

---
+++ config/AppEnv.php
@@ -0,0 +1,48 @@
+<?php
+namespace App\Config;
+
+use Dotenv\Dotenv;
+
+final class AppEnv
+{
+    public static function load(string $baseDir): void
+    {
+        $env = getenv('APP_ENV') ?: 'development';
+
+        switch ($env) {
+            case 'testing':
+                $file = '.env.testing';
+                break;
+            case 'development':
+                $file = '.env.dev';
+                break;
+            default:
+                $file = '.env';
+        }
+
+        $dotenv = Dotenv::createImmutable($baseDir, $file);
+        $dotenv->safeLoad();
+    }
+}
+
+// use Dotenv\Dotenv;
+
+// function loadEnv(): void {
+//     // Decide which env file to load based on APP_ENV
+//     $env = getenv('APP_ENV') ?: 'development';
+
+//     switch ($env) {
+//         case 'testing':
+//             $file = '.env.testing';
+//             break;
+//         case 'development':
+//             $file = '.env.dev';
+//             break;
+//         default:
+//             $file = '.env';
+//     }
+
+//     $dotenv = Dotenv::createImmutable(dirname(__DIR__), $file);
+//     $dotenv->safeLoad();
+// }
+
---
+++ config/constants.php
@@ -0,0 +1,2 @@
+<?php
+define('APP_ROOT', dirname(__DIR__));
