6612c7c30148d9da096e2588c1cfc25906c223b9
Gagan Rana <gaganrana.rana@gmail.com>
2026-03-21 14:29:19 +0530

    feat: Add logging into Home Controller

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -1,8 +1,23 @@
 <?php
 namespace App\Controllers;
 
+use App\Http\Response;
+use Monolog\Logger;
+
 class HomeController {
-    public function index(): void {
+    private Logger $logger;
+
+    public function __construct(Logger $logger) {
+        $this->logger = $logger;
+    }
+
+    public function index(): Response {
+        $this->logger->info("Home page accessed");
+
+        ob_start();
         require APP_ROOT . '/public/homePage.php';
+        $content = ob_get_clean();
+
+        return new Response(200, [], $content);
     }
 }
