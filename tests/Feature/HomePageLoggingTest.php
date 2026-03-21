2c0984b680727b13f1710f371a3543a3c7b962b6
Gagan Rana <gaganrana.rana@gmail.com>
2026-03-21 14:28:24 +0530

    test: Add fail test for logging

--- tests/Feature/HomePageLoggingTest.php
+++ tests/Feature/HomePageLoggingTest.php
@@ -1,20 +1,27 @@
 <?php
+declare(strict_types=1);
 
 namespace Tests\Feature;
 
 use PHPUnit\Framework\TestCase;
 use App\Controllers\HomeController;
+use App\Config\AppLogger;
 
-class HomePageLoggingTest extends TestCase
+final class HomePageLoggingTest extends TestCase
 {
-    public function test_home_page_access_should_be_logged()
+    public function test_home_page_access_should_be_logged(): void
     {
         $logFile = APP_ROOT . '/storage/logs/app.log';
         if (file_exists($logFile)) {
             unlink($logFile);
         }
 
-        $response = (new HomeController())->index();
+        // Manually inject the logger
+        $logger = AppLogger::getLogger();
+        $controller = new HomeController($logger);
+
+        $response = $controller->index();
+
         $this->assertStringContainsString('<title>My Test Home Page</title>', $response->getBody());
 
         $this->assertFileExists($logFile);
